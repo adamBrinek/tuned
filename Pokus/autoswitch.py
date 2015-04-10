a1a7d8687eaa5278b0d2e918bc56887b49159a3d
Adam Brinek <adam.brinek@gmail.com>
2015-04-10 13:57:04 +0200

    In Pokus/autoswitch.py i can read tuned-main.conf and get info about my auto switching profiles. With that info. I can creat table_1: which i will be using for checking dbus queries and for switching the profiles. Next step is make the DBUS connection and check all queries.

--- Pokus/autoswitch.py
+++ Pokus/autoswitch.py
@@ -1,29 +1,34 @@
 #!/usr/bin/python -Es
 
 from configobj import ConfigObj
+from validate import Validator
+import sys
 
-tab_1 = []
-pos = -1
 PROFILES = "Profiles"
 NAMES = "names"
 CHANGING = "changing"
 PRIORITY = "priority"
 PARENT = "parent"
 
-global_config_spec =    ["dynamic_tuning = boolean(default=%s)" % consts.CFG_DEF_DYNAMIC_TUNING,
-                        "sleep_interval = integer(default=%s)" % consts.CFG_DEF_SLEEP_INTERVAL,
-                        "update_interval = integer(default=%s)" % consts.CFG_DEF_UPDATE_INTERVAL]
+CFG_DEF_DYNAMIC_TUNING = True
+CFG_DEF_SLEEP_INTERVAL = 1
+CFG_DEF_UPDATE_INTERVAL = 10
+GLOBAL_CONFIG_FILE = "tuned-main.conf"
+
+global_config_spec =    ["dynamic_tuning = boolean(default=%s)" % CFG_DEF_DYNAMIC_TUNING,
+                        "sleep_interval = integer(default=%s)" % CFG_DEF_SLEEP_INTERVAL,
+                        "update_interval = integer(default=%s)" % CFG_DEF_UPDATE_INTERVAL]
 
 class Conf(object):
     def __init__(self,conf_name):
-        self.config = self._load_global_config()
+        self.config = self._load_global_config(conf_name)
 
 
-    def _load_global_config(self, file_name = consts.GLOBAL_CONFIG_FILE):
+    def _load_global_config(self, file_name = GLOBAL_CONFIG_FILE):
         """
         Loads global configuration file.
         """
-        log.debug("reading and parsing global configuration file '%s'" % consts.GLOBAL_CONFIG_FILE)
+#        log.debug("reading and parsing global configuration file '%s'" % GLOBAL_CONFIG_FILE)
     
         try:
             config = ConfigObj(file_name, configspec=global_config_spec, raise_errors = True, file_error = True)
@@ -40,42 +45,45 @@ class Conf(object):
         return config
 
 class Autoswitch(object):
-    def __init__(self, conf):
+    def __init__(self, conf=None):
         self._config = conf
+        self._tab_1 = []
+        self._pos = -1
+        self._load_default_values()
+        if self._config is not None:
+            self._load_values()
 
+    def _load_default_values(self):
+        self._sleep_interval = int(CFG_DEF_SLEEP_INTERVAL)
+        self._update_interval = int(CFG_DEF_UPDATE_INTERVAL)
+        self._dynamic_tuning = CFG_DEF_DYNAMIC_TUNING
 
+    def _load_values(self):
+        self._sleep_interval = int(self._config["sleep_interval"])
+        self._update_interval = int(self._config.get("update_interval", CFG_DEF_UPDATE_INTERVAL))
+        self._dynamic_tuning = self._config.get("dynamic_tuning", CFG_DEF_DYNAMIC_TUNING)
+        self._profile_names = self._config[PROFILES][NAMES]
+        
+        for item in self._profile_names:
+            self._pos += 1
+            self._tab_1.append([])
+            self._tab_1[self._pos].append(item)
+            self._tab_1[self._pos].append(self._config[item][PRIORITY])
+            self._tab_1[self._pos].append(self._config[item][PARENT])
+        
+        
+        print(self._tab_1[0])
+        print(self._tab_1[1])
 #        ------------------------------------
 #tab_1   | Code-name | Priority | Parent    |
 #        ------------------------------------
 #        | String    | Integer  | List      |
 #        ------------------------------------
-
-
-
-config = ConfigObj('tuned-main.conf')
-profiles = config[PROFILES][NAMES]
-for item in profiles:
-    pos += 1
-    tab_1.append([])
-    tab_1[pos].append(item)
-    tab_1[pos].append(config[item][PRIORITY])
-    tab_1[pos].append(config[item][PARENT])
-
-print (tab_1)
-
-
-self._sleep_interval = int(consts.CFG_DEF_SLEEP_INTERVAL)
-self._update_interval = int(consts.CFG_DEF_UPDATE_INTERVAL)
-self._dynamic_tuning = consts.CFG_DEF_DYNAMIC_TUNING
-
-if config is not None:
-    self._sleep_interval = int(config.get("sleep_interval", consts.CFG_DEF_SLEEP_INTERVAL))
-    self._update_interval = int(config.get("update_interval", consts.CFG_DEF_UPDATE_INTERVAL))
-    self._dynamic_tuning = config.get("dynamic_tuning", consts.CFG_DEF_DYNAMIC_TUNING)
-
-
-
-
+#config = ConfigObj('tuned-main.conf')
+#profiles = config[PROFILES][NAMES]
+#
 
 conf = Conf("tuned-main.conf")
-auto = Autoswitch(conf)
+auto = Autoswitch(conf.config)
+
+sys.exit(0)
